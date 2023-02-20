86823e47fa81ac71e526431909aa620c42bf9a72
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-02-21 01:29:47 +0300

    init

---
+++ src/main/kotlin/com/ivan/blpslab1/BlpsLab1Application.kt
@@ -0,0 +1,11 @@
+package com.ivan.blpslab1
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BlpsLab1Application
+
+fun main(args: Array<String>) {
+    runApplication<BlpsLab1Application>(*args)
+}
