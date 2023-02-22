1b9010ee31a74a6116352f499e4675b33cafe2b1
erik_101 <erik_password228>
2023-02-22 16:48:13 +0300

    update config

--- script/init-env-variables.bash
+++ script/init-env-variables.bash
@@ -3,3 +3,4 @@ export DATASOURCE_URL=jdbc:postgresql://localhost:5432/studs
 export DATASOURCE_USERNAME=s312989
 export DATASOURCE_PASSWORD=1ECFMTK95AEQiVgR
 export PAYMENT_INTEGRATION_URL=http://localhost:9090/api/v1/payment/
+export X_EDX_API_KEY=PASHAGAY
