c759372e1b203031313fb03d70834ded4dac535b
Ivan <ivan.kustarve@gmail.com>
2023-02-22 15:32:21 +0300

    fix remote

--- src/main/kotlin/com/iver/blpslab1/remote/retrofit2/payment/PayRequest.kt
+++
@@ -1,5 +0,0 @@
-package com.iver.blpslab1.remote.retrofit2.payment
-
-data class PayRequest(
-    val coast: Float,
-)
--- src/main/kotlin/com/iver/blpslab1/remote/retrofit2/payment/PaymentIntegration.kt
+++
@@ -1,5 +0,0 @@
-package com.iver.blpslab1.remote.retrofit2.payment
-
-interface PaymentIntegration {
-    fun pay(payRequest: PayRequest)
-}
