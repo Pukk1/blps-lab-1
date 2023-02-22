427ad3265b22be3e8b01a617ff030b3b20dddfeb
erik_101 <erik_password228>
2023-02-22 16:47:06 +0300

    update config

---
+++ script/init-env-variables.bash
@@ -0,0 +1,5 @@
+export SERVER_PORT=8080
+export DATASOURCE_URL=jdbc:postgresql://localhost:5432/studs
+export DATASOURCE_USERNAME=s312989
+export DATASOURCE_PASSWORD=1ECFMTK95AEQiVgR
+export PAYMENT_INTEGRATION_URL=http://localhost:9090/api/v1/payment/
