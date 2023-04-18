3ab238034d828b8eb73e50dee6cb0fa8e28a036e
mobest1an <erikk202@gmail.com>
2023-04-19 02:16:29 +0300

    add atomikos TM

--- src/main/kotlin/com/iver/blpslab1/config/AtomikosConfig.kt
+++ src/main/kotlin/com/iver/blpslab1/config/AtomikosConfig.kt
@@ -1,6 +1,6 @@
 package com.iver.blpslab1.config
 
-import com.atomikos.icatch.config.UserTransactionServiceImp
+//import com.atomikos.icatch.config.UserTransactionServiceImp
 import com.atomikos.icatch.jta.UserTransactionManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
