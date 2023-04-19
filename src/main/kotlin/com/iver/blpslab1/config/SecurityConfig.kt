94a42b375ef71f5fef7b3e64305c3a38fa8459c4
Ivan <ivan.kustarve@gmail.com>
2023-04-19 19:58:33 +0300

    velichayshaya.2

--- src/main/kotlin/com/iver/blpslab1/config/SecurityConfig.kt
+++ src/main/kotlin/com/iver/blpslab1/config/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.iver.blpslab1.config
 
+import com.iver.blpslab1.dao.user.Role
 import com.iver.blpslab1.dao.user.UserRepository
 import com.iver.blpslab1.security.UserDetailsServiceImpl
 import org.springframework.context.annotation.Bean
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 
@@ -20,21 +22,11 @@ class SecurityConfig(
     private val userRepository: UserRepository,
 ) : WebSecurityConfigurerAdapter() {
 
-//    override fun configure(http: HttpSecurity) {
-//        http
-//            .authorizeRequests()
-//            .antMatchers("/**").permitAll()
-//            .and()
-//            .authorizeRequests()
-//            .anyRequest()
-//            .authenticated()
-//    }
-
     override fun configure(http: HttpSecurity) {
-        http
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
             .csrf().disable()
             .authorizeRequests()
-            .antMatchers("/api/**").authenticated()
+            .antMatchers("/api/**").authenticated()/*.hasAnyAuthority(Role.ADMIN.name)*/
             .and()
             .formLogin()
             .and()
@@ -65,17 +57,3 @@ class SecurityConfig(
             .userDetailsService(myUserDetailsService())
     }
 }
-
-//class MyUserDetailsService : UserDetailsService {
-//    override fun loadUserByUsername(username: String): UserDetails {
-//        return if ("user" == username) {
-//            User.builder()
-//                .username("user")
-//                .password("\$2a\$10\$mbWxyHyT0hCkhp7ME4tE3.rBsUdVYQFrReMULu9KQLxbrtBvrIz0K") // password: password
-//                .roles("USER")
-//                .build()
-//        } else {
-//            throw UsernameNotFoundException("User not found")
-//        }
-//    }
-//}
