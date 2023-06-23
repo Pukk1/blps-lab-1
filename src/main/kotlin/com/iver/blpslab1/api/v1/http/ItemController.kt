fd79b88fbd23fa50e9f3c503b848d6530c252d26
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-06-23 18:50:21 +0300

    return ItemController

--- src/main/kotlin/com/iver/blpslab1/api/v1/http/ItemController.kt
+++ src/main/kotlin/com/iver/blpslab1/api/v1/http/ItemController.kt
@@ -5,10 +5,8 @@ import com.iver.blpslab1.api.v1.http.requests.UpdateItemRequest
 import com.iver.blpslab1.api.v1.http.views.ItemView
 import com.iver.blpslab1.api.v1.http.views.toView
 import com.iver.blpslab1.service.item.ItemService
-import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import javax.validation.constraints.NotBlank
 
 @RestController
 @RequestMapping("/api/v1/item")
@@ -16,25 +14,25 @@ class ItemController(
     private val itemService: ItemService
 ) {
 
-//    @PostMapping
-//    @ResponseStatus(HttpStatus.CREATED)
-//    fun createItem(
-//        @RequestBody createItemRequest: CreateItemRequest,
-//    ): ItemView = itemService.createItem(createItemRequest).toView()
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createItem(
+        @RequestBody createItemRequest: CreateItemRequest,
+    ): ItemView = itemService.createItem(createItemRequest).toView()
 
-//    @GetMapping("/{itemId}")
-//    fun getItemById(
-//        @PathVariable itemId: Long,
-//    ): ItemView = itemService.getItemById(itemId).toView()
+    @GetMapping("/{itemId}")
+    fun getItemById(
+        @PathVariable itemId: Long,
+    ): ItemView = itemService.getItemById(itemId).toView()
 
-//    @PutMapping("/{itemId}")
-//    fun updateItem(
-//        @PathVariable itemId: Long,
-//        @RequestBody updateItemRequest: UpdateItemRequest,
-//    ): ItemView = itemService.updateItem(itemId, updateItemRequest).toView()
+    @PutMapping("/{itemId}")
+    fun updateItem(
+        @PathVariable itemId: Long,
+        @RequestBody updateItemRequest: UpdateItemRequest,
+    ): ItemView = itemService.updateItem(itemId, updateItemRequest).toView()
 
-//    @DeleteMapping("/{itemId}")
-//    fun deleteItem(
-//        @PathVariable itemId: Long,
-//    ) = itemService.deleteItem(itemId)
+    @DeleteMapping("/{itemId}")
+    fun deleteItem(
+        @PathVariable itemId: Long,
+    ) = itemService.deleteItem(itemId)
 }
